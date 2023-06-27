5c5cedb0056aa6932c7bdfda542cdaf027d84030
Raydioscope <arijitray.prof@gmail.com>
2023-06-27 15:27:24 +0530

    swagger for ocelot

--- Ocelotdemo/Program.cs
+++ Ocelotdemo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -9,18 +11,27 @@ builder.Configuration.AddJsonFile("ocelot.json", optional:false, reloadOnChange:
 builder.Services.AddOcelot(builder.Configuration);
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 
 
 var app = builder.Build();
+app.UseSwaggerForOcelotUI();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 await app.UseOcelot();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+   
 }
 app.UseStaticFiles();
+
 
 app.UseRouting();
 
